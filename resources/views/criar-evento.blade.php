c23c268f5d8b759d7af2bd16958d8274513e7528
Marcus <marcus.vieiraleal94@gmail.com>
2024-02-05 01:00:36 -0300

    Adicionado upload de imagem ao form

--- resources/views/criar-evento.blade.php
+++ resources/views/criar-evento.blade.php
@@ -2,8 +2,14 @@
 @section('title', 'Etiqueta Livre') {{-- Define o Titulo da Pagina --}}
 @section('content') {{-- Define o conteudo da pagina --}}
 
-    <form>
+    <form action="/cadastrar-ebooks" method="post" enctype="multipart/form-data">
         @csrf {{-- Sempre colocar em formularios --}}
+
+        <div class="mb-3">
+            <label for="image">Capa do Livro:</label>
+            <input type="file" id="image" name="image" class="from-control-file">
+        </div>
+
         <div class="mb-3">
             <label class="form-label">Titulo</label>
             <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Coloque o titulo">
@@ -16,8 +22,9 @@
 
         <div class="form-outline">
             <label class="form-label">Descrição</label><br>
-            <textarea class="form-label" name="descricao" id="descricao" rows="4"></textarea>
+            <textarea class="textarea" name="descricao" id="descricao"></textarea>
         </div>
+
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
     
